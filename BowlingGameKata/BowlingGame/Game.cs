23d710ed55c397eba039b03ec26f9178190029bb
czarlestra <charles.lanada@gmail.com>
2020-02-22 18:51:09 +0800

    finished bowling game kata

--- BowlingGameKata/BowlingGame/Game.cs
+++ BowlingGameKata/BowlingGame/Game.cs
@@ -1,20 +1,72 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Drawing;
+using System.Linq;
 
 namespace BowlingGame
 {
     public class Game
     {
+        private int CurrentRole;
+        private readonly int[] Rolls = new int[21];
+
         public void Roll(int pins)
         {
-            score += pins;
+            Rolls[CurrentRole] = pins;
+            CurrentRole++;
         }
-
-        private int score { get; set; }
 
         public int Score()
         {
+            var score = 0;
+            var frameIndex = 0;
+            for (var frame = 0; frame < 10; frame++)
+            {
+                if (IsStrike(frameIndex))
+                {
+                    score += StrikeBonus(frameIndex);
+                    frameIndex += 1;
+                }
+                else if (IsSpare(frameIndex))
+                {
+                    score += SpareBonus(frameIndex);
+                    frameIndex += 2;
+                }
+                else
+                {
+                    score += SumOfBallsInFrame(frameIndex);
+                    frameIndex += 2;
+                }
+
+                Console.WriteLine($"frame # {frame + 1}: {score}");
+            }
+
             return score;
+        }
+
+        private bool IsStrike(int frameChanceIndex)
+        {
+            return Rolls[frameChanceIndex] == 10;
+        }
+
+        private int SumOfBallsInFrame(int frameChanceIndex)
+        {
+            return Rolls[frameChanceIndex] + Rolls[frameChanceIndex + 1];
+        }
+
+        private int SpareBonus(int frameChanceIndex)
+        {
+            return 10 + Rolls[frameChanceIndex + 2];
+        }
+
+        private int StrikeBonus(int frameChanceIndex)
+        {
+            return 10 + Rolls[frameChanceIndex + 1] + Rolls[frameChanceIndex + 2];
+        }
+
+        bool IsSpare(int i)
+        {
+            return Rolls[i] + Rolls[i + 1] == 10;
         }
     }
 }
--- BowlingGameKata/BowlingGameTests/GameTests.cs
+++ BowlingGameKata/BowlingGameTests/GameTests.cs
@@ -6,9 +6,9 @@ namespace BowlingGameTests
 {
     public class GameTests : With_an_automocked<Game>
     {
-        private void RollMany(int n, int pins)
+        private void RollMany(int numberOfRoll, int pins)
         {
-            for (var i = 0; i < n; i++)
+            for (var i = 0; i < numberOfRoll; i++)
             {
                 ClassUnderTest.Roll(pins);
             }
@@ -17,21 +17,87 @@ namespace BowlingGameTests
         [Test]
         public void When_game_is_on_test_gutter()
         {
-            var n = 20;
+            var game = ClassUnderTest;
+            var numberOfRoll = 20;
             var pins = 0;
-            RollMany(n, pins);
-            Assert.That(0, Is.EqualTo(ClassUnderTest.Score()));
+            RollMany(numberOfRoll, pins);
+            Assert.That(game.Score(),  Is.EqualTo(0));
         }
 
         [Test]
         public void When_game_is_on_test_all_ones()
         {
+            var game = ClassUnderTest;
             var pins = 1;
-            var n = 20;
-            RollMany(n, pins);
-            Assert.That(20, Is.EqualTo(ClassUnderTest.Score()));
+            var numberOfRoll = 20;
+            RollMany(numberOfRoll, pins);
+            Assert.That(game.Score(), Is.EqualTo(20));
         }
-        
-        
+
+        [Test]
+        public void When_game_is_on_test_one_spare()
+        {
+            var game = ClassUnderTest;
+            RollSpare(game);
+            game.Roll(3);
+            RollMany(17, 0);
+            Assert.That(game.Score(),Is.EqualTo(16));
+        }
+
+        [Test]
+        public void When_game_is_on_test_one_strike()
+        {
+            var game = ClassUnderTest;
+            RollStrike(game);
+            game.Roll(3);
+            game.Roll(4);
+            RollMany(16,0);
+            Assert.That(game.Score(), Is.EqualTo(24));
+        }
+
+        [Test]
+        public void When_game_is_on_test_two_strike()
+        {
+            var game = ClassUnderTest;
+            RollStrike(game);
+            RollStrike(game);
+            game.Roll(3);
+            game.Roll(4);
+            RollMany(14,0);
+            Assert.That(game.Score(), Is.EqualTo(47));
+        }
+
+        [Test]
+        public void When_game_is_on_test_two_strike_two_spare()
+        {
+            var game = ClassUnderTest;
+            RollStrike(game);
+            RollSpare(game);
+            RollStrike(game);
+            RollSpare(game);
+            game.Roll(3);
+            game.Roll(4);
+            RollMany(10,0);
+            Assert.That(game.Score(), Is.EqualTo(80));
+        }
+
+        [Test]
+        public void When_game_is_on_test_perfect_game()
+        {
+            RollMany(12, 10);
+            var game = ClassUnderTest;
+            Assert.That(game.Score(), Is.EqualTo(300));
+        }
+
+        private static void RollStrike(Game game)
+        {
+            game.Roll(10);
+        }
+
+        private void RollSpare(Game game)
+        {
+            game.Roll(5);
+            game.Roll(5);
+        }
     }
 }
